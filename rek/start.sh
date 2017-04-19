ee7a5d7b851670bf2c74d0d47f04c86998837f14
Richard Brandão <richardsbrandao@gmail.com>
2017-04-19 13:49:48 -0300

    :it: start sh in /dev/null

--- rek/start.sh
+++ rek/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 RAILS_ENV=production bundle exec rake assets:precompile
-SECRET_KEY_BASE=$(rake secret) rails s Puma -e production -b 0.0.0.0
+SECRET_KEY_BASE=$(rake secret) rails s Puma -e production -b 0.0.0.0 -p 80 > /dev/null
